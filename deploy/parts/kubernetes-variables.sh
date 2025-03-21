200390668401cd37ee1860cb89d18227546c5e8f
Jan Górecki <jan.gorecki@shopsys.com>
2025-03-21 10:30:36 +0100

    add annotations for new logging

--- deploy/parts/kubernetes-variables.sh
+++ deploy/parts/kubernetes-variables.sh
@@ -5,12 +5,25 @@ echo -n "Prepare Kubernetes variables "
 assertVariable "RUNNING_PRODUCTION"
 assertVariable "CONFIGURATION_TARGET_PATH"
 assertVariable "S3_ENDPOINT"
+assertVariable "PROJECT_NAME"
 
 if [ -z ${REDIS_VERSION} ]; then
   REDIS_VERSION='redis:7.4-alpine'
 fi
 
 VARS+=(REDIS_VERSION)
+
+if [[ $PROJECT_NAME != *"-"* ]]; then
+    echo -e "[${RED}ERROR${NO_COLOR}] PROJECT_NAME must contain a dash to separate project and environment"
+    exit 1
+fi
+
+projectNameSplit=(${PROJECT_NAME//-/ })
+NAME_OF_PROJECT=${projectNameSplit[0]}
+PROJECT_ENVIRONMENT=${projectNameSplit[1]}
+
+VARS+=(NAME_OF_PROJECT)
+VARS+=(PROJECT_ENVIRONMENT)
 
 DOMAINS_URLS_FILEPATH="$(find_file "config" "domains_urls")"
 
