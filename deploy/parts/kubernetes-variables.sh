904581589b26f2f06d711a3e2a678464fc18ec15
Tomáš Ludvik <ludviktomas@gmail.com>
2026-09-09 10:35:00 +0200

    quote version variables in defaulting checks to avoid word-splitting

--- deploy/parts/kubernetes-variables.sh
+++ deploy/parts/kubernetes-variables.sh
@@ -7,13 +7,13 @@ assertVariable "CONFIGURATION_TARGET_PATH"
 assertVariable "S3_ENDPOINT"
 assertVariable "PROJECT_NAME"
 
-if [ -z ${REDIS_VERSION} ]; then
+if [ -z "${REDIS_VERSION}" ]; then
   REDIS_VERSION='redis:7.4-alpine'
 fi
 
 VARS+=(REDIS_VERSION)
 
-if [ -z ${GOTENBERG_VERSION} ]; then
+if [ -z "${GOTENBERG_VERSION}" ]; then
   GOTENBERG_VERSION='gotenberg/gotenberg:8'
 fi
 
